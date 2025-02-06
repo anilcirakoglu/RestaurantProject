73e20f30b351251d5be09480899d9c3cda56d5ac
ANIL CIRAKOGLU <anil.cirakoglu@isik.edu.tr>
2025-02-07 02:40:52 +0300

    Admin UI added

---
+++ RestaurantUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
